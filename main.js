9915e868e1a2974317d266f578458a555d677807
Michael Claborn <mclaborn2010@gmail.com>
2019-08-03 16:16:27 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text of current word
+  const fulltxt = this.words[current];
+  // Check if deleting
+  if (this.isDeleting) {
+    // Remove char
+    this.txt = fulltxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add char
+    this.txt = fulltxt.substring(0, this.txt.length + 1);
+  }
+
+  // Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  setTimeout(() => this.type(), 500);
+};
+
+// Init On Dom Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
